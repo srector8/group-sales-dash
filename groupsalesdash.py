db48a68e4f6774069c35c0e342409e9581efa0f2
srector8 <157165984+srector8@users.noreply.github.com>
2024-07-02 16:48:18 -0400

    Update groupsalesdash.py

--- groupsalesdash.py
+++ groupsalesdash.py
@@ -446,11 +446,15 @@ else:
             # Calculate percentage of sales for each rep for each game
             sales_distribution['sales_percentage'] = sales_distribution.groupby('event_name_display')['block_full_price'].transform(lambda x: (x / x.sum()) * 100)
 
+            event_order = sorted(sales_distribution['event_name_display'].unique())
+
+
             # Bar chart for sales distribution by rep for each game
             bar_chart_sales_dist = alt.Chart(sales_distribution).mark_bar().encode(
                 x=alt.X('event_name_display:N', axis=alt.Axis(title='Game')),
                 y=alt.Y('sales_percentage:Q', stack='normalize', axis=alt.Axis(format='%'), title='Sales Percentage'),
                 color=alt.Color('acct_rep_full_name:N', legend=alt.Legend(title='Account Rep')),
+                order=alt.Order('sales_percentage:Q', sort='descending'),
                 tooltip=['event_name_display:N', 'acct_rep_full_name:N', 'block_full_price:Q', 'sales_percentage:Q']
             ).properties(
                 width=800,
